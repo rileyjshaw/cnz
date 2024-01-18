eb3fa2cef4a6861d9b0d43d13c44fa073e95e96c
Riley Shaw <rileyjshaw@gmail.com>
2024-01-18 09:59:34 -0800

    Improve object detection method to reduce size

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-let O=Object
-export default (...a)=>a.flatMap(x=>x&&O.getPrototypeOf(x)===O.prototype?O.entries(x).map(([k,v])=>v&&k):x).filter(s=>s&&s.trim).join(' ')
+export default (...a)=>a.flatMap(x=>x?.constructor==Object?Object.keys(x).filter(k=>x[k]):x).filter(s=>s&&s.trim).join(' ')
